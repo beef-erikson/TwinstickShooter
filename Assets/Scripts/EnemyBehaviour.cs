6d15bfcb7ae86532dce16e503322655a4af87531
Troy Martin <beef.erikson.studios@gmail.com>
2019-06-22 04:32:25 -0400

    Enemy logic
    Health and death of enemy. Also removed unneeded namespaces from MoveTowardsPlayer.

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    #region Fields
+
+    public int Health = 2;
+
+    #endregion
+
+    #region Methods
+    
+    /// <summary>
+    /// Takes away health and destroys if dead
+    /// </summary>
+    /// <param name="collision"></param>
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Hit by laser
+        if (collision.gameObject.name.Contains("Laser"))
+        {
+            ShotBehaviour shot = collision.gameObject.GetComponent<ShotBehaviour>();
+            Health -= shot.shotDamage;
+            Destroy(collision.gameObject);
+        }
+
+        if (Health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    #endregion
+}
