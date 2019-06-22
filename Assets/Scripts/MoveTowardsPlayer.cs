ca721e8563e53ce1c1ac834201568b476353dd5a
Troy Martin <beef.erikson.studios@gmail.com>
2019-06-22 04:09:34 -0400

    Enemy added - chases player
    Note this script is only for chasing after the player; the enemy will have it's own script for behaviour

---
+++ Assets/Scripts/MoveTowardsPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowardsPlayer : MonoBehaviour
+{
+    #region Fields
+
+    Transform player;
+    public float speed = 2.0f;
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Finds the player and saves its transform
+    /// </summary>
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;
+    }
+
+    /// <summary>
+    /// Chases the player
+    /// </summary>
+    void Update()
+    {
+        Vector3 delta = player.position - transform.position;
+        delta.Normalize();
+        float moveSpeed = speed * Time.deltaTime;
+        transform.position = transform.position + (delta * moveSpeed);
+    }
+
+    #endregion
+}
